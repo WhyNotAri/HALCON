00ea0fa40c5df66572887a79c04d3ce60f106528
Ari Morales <arimorales2506@gmail.com>
2026-03-24 22:46:05 -0600

    added orders crud

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'invoice_number',
+        'customer_id',
+        'status',
+        'order_date',
+        'delivery_address',
+        'notes'
+    ];
+
+    public function costumer() {
+        return $this->belongsTo(Costumer::class);
+    }
+}
